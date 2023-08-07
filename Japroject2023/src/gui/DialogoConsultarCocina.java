8eb67a91c31a551191a5177a72b89f5805fc4921
ipanaque94 <rodasenoc94@gmail.com>
2023-08-06 19:52:48 -0500

    mensaje

--- Japroject2023/src/gui/DialogoConsultarCocina.java
+++ Japroject2023/src/gui/DialogoConsultarCocina.java
@@ -8,8 +8,13 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ActionEvent;
+import javax.swing.DefaultComboBoxModel;
 
-public class DialogoConsultarCocina extends JDialog {
+public class DialogoConsultarCocina extends JDialog implements ActionListener, ItemListener {
 
 	/**
 	 * 
@@ -22,7 +27,7 @@ public class DialogoConsultarCocina extends JDialog {
 	private JLabel lblAlto;
 	private JLabel lblFondo;
 	private JLabel lblQuemadores;
-	private JComboBox<Object> comboBox;
+	private JComboBox<String> cboModelo;
 	private JTextField txtPrecio;
 	private JTextField txtAncho;
 	private JTextField txtAlto;
@@ -55,62 +60,173 @@ public class DialogoConsultarCocina extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		lblModelo = new JLabel("Modelo");
 		lblModelo.setBounds(10, 29, 73, 14);
 		contentPanel.add(lblModelo);
-		
+
 		lblPrecio = new JLabel("Precio (S/)");
 		lblPrecio.setBounds(10, 54, 73, 14);
 		contentPanel.add(lblPrecio);
-		
+
 		lblAncho = new JLabel("Ancho (cm)");
 		lblAncho.setBounds(10, 79, 73, 14);
 		contentPanel.add(lblAncho);
-		
+
 		lblAlto = new JLabel("Alto (cm)");
 		lblAlto.setBounds(10, 104, 73, 14);
 		contentPanel.add(lblAlto);
-		
+
 		lblFondo = new JLabel("Fondo (cm)");
 		lblFondo.setBounds(10, 129, 73, 14);
 		contentPanel.add(lblFondo);
-		
+
 		lblQuemadores = new JLabel("Quemadores");
 		lblQuemadores.setBounds(10, 154, 83, 14);
 		contentPanel.add(lblQuemadores);
-		
-		comboBox = new JComboBox<Object>();
-		comboBox.setBounds(103, 26, 135, 20);
-		contentPanel.add(comboBox);
-		
+
+		cboModelo = new JComboBox<String>();
+		cboModelo.setModel(new DefaultComboBoxModel<String>(
+				new String[] { "Mabe EMP6120PG0", "Indurama Parma", "Sole COSOL027", "Coldex CX602", "Reco Dakota" }));
+		cboModelo.setBounds(103, 26, 135, 20);
+		contentPanel.add(cboModelo);
+
 		txtPrecio = new JTextField();
+		txtPrecio.setEditable(false);
 		txtPrecio.setBounds(103, 51, 135, 20);
 		contentPanel.add(txtPrecio);
 		txtPrecio.setColumns(10);
-		
+
 		txtAncho = new JTextField();
+		txtAncho.setEditable(false);
 		txtAncho.setColumns(10);
 		txtAncho.setBounds(103, 76, 135, 20);
 		contentPanel.add(txtAncho);
-		
+
 		txtAlto = new JTextField();
+		txtAlto.setEditable(false);
 		txtAlto.setColumns(10);
 		txtAlto.setBounds(103, 101, 135, 20);
 		contentPanel.add(txtAlto);
-		
+
 		txtFondo = new JTextField();
+		txtFondo.setEditable(false);
 		txtFondo.setColumns(10);
 		txtFondo.setBounds(103, 126, 135, 20);
 		contentPanel.add(txtFondo);
-		
+
 		txtQuemadores = new JTextField();
+		txtQuemadores.setEditable(false);
 		txtQuemadores.setColumns(10);
 		txtQuemadores.setBounds(103, 151, 135, 20);
 		contentPanel.add(txtQuemadores);
-		
+
+		txtPrecio.setText(Tienda1.precio0 + "");
+		txtAncho.setText(Tienda1.ancho0 + "");
+		txtAlto.setText(Tienda1.alto0 + "");
+		txtFondo.setText(Tienda1.fondo0 + "");
+		txtQuemadores.setText(Tienda1.quemadores0 + "");
+
 		btnCerrar = new JButton("Cerrar");
+		btnCerrar.addActionListener(this);
 		btnCerrar.setBounds(309, 25, 89, 23);
 		contentPanel.add(btnCerrar);
+
 	}
+
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == btnCerrar) {
+			actionPerformedBtnCerrar(arg0);
+
+		}
+	}
+
+	protected void actionPerformedBtnCerrar(ActionEvent arg0) {
+		this.dispose();
+
+	}
+
+	public void itemStateChanged1(ItemEvent arg0) {
+		if (arg0.getSource() == cboModelo) {
+			itemStateChangedCboModelo(arg0);
+		}
+	}
+
+	protected void itemStateChangedCboModelo(ItemEvent arg0) {
+		int modelo;
+
+		modelo = cboModelo.getSelectedIndex();
+
+		switch (modelo) {
+		case 0:
+		
+			// consultarModelo(Tienda1.precio0, Tienda1.ancho0, Tienda1.alto0, Tienda1.fondo0, Tienda1.quemadores0);
+			break;
+		case 1:
+			consultarModelo(Tienda1.precio1, Tienda1.ancho1, Tienda1.alto1, Tienda1.fondo1, Tienda1.quemadores1);
+			break;
+		case 2:
+			consultarModelo(Tienda1.precio2, Tienda1.ancho2, Tienda1.alto2, Tienda1.fondo2, Tienda1.quemadores2);
+			break;
+		case 3:
+			consultarModelo(Tienda1.precio3, Tienda1.ancho3, Tienda1.alto3, Tienda1.fondo3, Tienda1.quemadores3);
+			break;
+		default:
+			consultarModelo(Tienda1.precio4, Tienda1.ancho4, Tienda1.alto4, Tienda1.fondo4, Tienda1.quemadores4);
+			break;
+		}
+	}
+
+	int leerModelo() {
+		
+		return cboModelo.getSelectedIndex();
+	}
+
+	public void itemStateChanged(ItemEvent arg0) {
+
+	}
+
+	void consultarModelo(double precio, double ancho, double alto, double fondo, int quemadores) {
+		txtPrecio.setText(precio + "");
+		txtAncho.setText(ancho + "");
+		txtAlto.setText(alto + "");
+		txtFondo.setText(fondo + "");
+		txtQuemadores.setText(quemadores + "");
+	}
+	// Datos mínimos de la primera cocina
+		public static String modelo0 = "Mabe EMP6120PG0";
+		public static double precio0 = 949.0;
+		
+		public static double fondo0 = 58.6;
+		public static double ancho0 = 60.0;
+		public static double alto0 = 91.0;
+		public static int quemadores0 = 4;
+		// Datos mínimos de la segunda cocina
+		public static String modelo1 = "Indurama Parma";
+		public static double precio1 = 1089.0;
+		public static double ancho1 = 80.0;
+		public static double alto1 = 94.0;
+		public static double fondo1 = 67.5;
+		public static int quemadores1 = 6;
+		// Datos mínimos de la tercera cocina
+		public static String modelo2 = "Sole COSOL027";
+		public static double precio2 = 850.0;
+		public static double ancho2 = 60.0;
+		public static double alto2 = 90.0;
+		public static double fondo2 = 50.0;
+		public static int quemadores2 = 4;
+		// Datos mínimos de la cuarta cocina
+		public static String modelo3 = "Coldex CX602";
+		public static double precio3 = 629.0;
+		public static double ancho3 = 61.6;
+		public static double alto3 = 95.0;
+		public static double fondo3 = 51.5;
+		public static int quemadores3 = 5;
+		// Datos mínimos de la quinta cocina
+		public static String modelo4 = "Reco Dakota";
+		public static double precio4 = 849.0;
+		public static double ancho4 = 75.4;
+		public static double alto4 = 94.5;
+		public static double fondo4 = 66.0;
+		public static int quemadores4 = 5;
 }
---
+++ Japroject2023/src/gui/Tienda1.java
@@ -0,0 +1,176 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.UIManager;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class Tienda1 extends JFrame implements ActionListener {
+	
+	// Datos mínimos de la primera cocina
+	public static String modelo0 = "Mabe EMP6120PG0";
+	public static double precio0 = 949.0;
+	
+	public static double fondo0 = 58.6;
+	public static double ancho0 = 60.0;
+	public static double alto0 = 91.0;
+	public static int quemadores0 = 4;
+	// Datos mínimos de la segunda cocina
+	public static String modelo1 = "Indurama Parma";
+	public static double precio1 = 1089.0;
+	public static double ancho1 = 80.0;
+	public static double alto1 = 94.0;
+	public static double fondo1 = 67.5;
+	public static int quemadores1 = 6;
+	// Datos mínimos de la tercera cocina
+	public static String modelo2 = "Sole COSOL027";
+	public static double precio2 = 850.0;
+	public static double ancho2 = 60.0;
+	public static double alto2 = 90.0;
+	public static double fondo2 = 50.0;
+	public static int quemadores2 = 4;
+	// Datos mínimos de la cuarta cocina
+	public static String modelo3 = "Coldex CX602";
+	public static double precio3 = 629.0;
+	public static double ancho3 = 61.6;
+	public static double alto3 = 95.0;
+	public static double fondo3 = 51.5;
+	public static int quemadores3 = 5;
+	// Datos mínimos de la quinta cocina
+	public static String modelo4 = "Reco Dakota";
+	public static double precio4 = 849.0;
+	public static double ancho4 = 75.4;
+	public static double alto4 = 94.5;
+	public static double fondo4 = 66.0;
+	public static int quemadores4 = 5;
+	// Porcentajes de descuento
+	public static double porcentaje1 = 7.5;
+	public static double porcentaje2 = 10.0;
+	public static double porcentaje3 = 12.5;
+	public static double porcentaje4 = 15.0;
+	// Obsequios
+	public static String obsequio1 = "Cafetera";
+	public static String obsequio2 = "Licuadora";
+	public static String obsequio3 = "Extractor";
+	// Cantidad óptima de unidades vendidas
+	public static int cantidadOptima = 30;
+	// Cuota diaria
+	public static double cuotaDiaria = 75000;
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JMenuBar menuBar;
+	private JMenu mnArchivo;
+	private JMenuItem mntmSalir;
+	private JMenu mnMantenimiento;
+	private JMenuItem mntmNewMenuItem;
+	private JMenuItem mntmNewMenuItem_1;
+	private JMenuItem mntmListarCocina;
+	private JMenu mnVentas;
+	private JMenuItem mntmVender;
+	private JMenuItem mntmGenerarReportes;
+	private JMenu mnConfiguracin;
+	private JMenuItem mntmConfigurar;
+	private JMenuItem mntmNewMenuItem_2;
+	private JMenuItem mntmNewMenuItem_3;
+	private JMenuItem mntmNewMenuItem_4;
+	private JMenu mnAyuda;
+	private JMenuItem mntmAcercaDeTienda;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tienda1 frame = new Tienda1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Tienda1() {
+		setTitle("Tienda");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 627, 418);
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		mntmSalir = new JMenuItem("Salir");
+		mntmSalir.addActionListener(this);
+		mnArchivo.add(mntmSalir);
+		
+		mnMantenimiento = new JMenu("Mantenimiento");
+		menuBar.add(mnMantenimiento);
+		
+		mntmNewMenuItem = new JMenuItem("Consultar Cocina");
+		mnMantenimiento.add(mntmNewMenuItem);
+		
+		mntmNewMenuItem_1 = new JMenuItem("Modificar Cocina");
+		mnMantenimiento.add(mntmNewMenuItem_1);
+		
+		mntmListarCocina = new JMenuItem("Listar Cocinas");
+		mnMantenimiento.add(mntmListarCocina);
+		
+		mnVentas = new JMenu("Ventas");
+		menuBar.add(mnVentas);
+		
+		mntmVender = new JMenuItem("Vender");
+		mnVentas.add(mntmVender);
+		
+		mntmGenerarReportes = new JMenuItem("Generar Reportes");
+		mnVentas.add(mntmGenerarReportes);
+		
+		mnConfiguracin = new JMenu("Configuraci\u00F3n");
+		menuBar.add(mnConfiguracin);
+		
+		mntmConfigurar = new JMenuItem("Configurar  Descuentos");
+		mnConfiguracin.add(mntmConfigurar);
+		
+		mntmNewMenuItem_3 = new JMenuItem("Configurar Obsequios");
+		mnConfiguracin.add(mntmNewMenuItem_3);
+		
+		mntmNewMenuItem_2 = new JMenuItem("Configurar cantidad \u00F3ptima");
+		mnConfiguracin.add(mntmNewMenuItem_2);
+		
+		mntmNewMenuItem_4 = new JMenuItem("Configurar cuota diaria");
+		mnConfiguracin.add(mntmNewMenuItem_4);
+		
+		mnAyuda = new JMenu("Ayuda");
+		menuBar.add(mnAyuda);
+		
+		mntmAcercaDeTienda = new JMenuItem("Acerca de tienda");
+		mnAyuda.add(mntmAcercaDeTienda);
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		if (arg0.getSource() == mntmSalir) {
+			actionPerformedMntmSalir(arg0);
+		}
+	}
+	protected void actionPerformedMntmSalir(ActionEvent arg0) {
+		System.exit(0);
+	}
+}
